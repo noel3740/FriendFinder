761da2946e01c9c0a2501a4afc1f25aac95ff3d8
Noel Nevarez <noel3740@gmail.com>
2019-01-15 10:38:38 -0600

    Setting up server.js code.

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 //Add dependencies
 const express = require("express");
-const path = require("path");
+
+//Set up the express app
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Set up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Server Routes
+var apiRoutes=require("./app/routing/apiRoutes");
+apiRoutes(app);
+var htmlRoutes=require("./app/routing/htmlRoutes");
+htmlRoutes(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
