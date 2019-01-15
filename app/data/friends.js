8d9896cfbd3a942266cb2425e971e32ed7f588e6
Noel Nevarez <noel3740@gmail.com>
2019-01-15 15:11:47 -0600

    Adding default data to friends array

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+// Friends data
+let fiends = [
+    {
+        "routeName": "bradpitt",
+        "name":"Brad Pitt",
+        "photo":"https://cdn1.thr.com/sites/default/files/imagecache/portrait_300x450/2012/05/brad_pitt_chanel_a_p.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      }
+];
+
+module.exports = fiends;
