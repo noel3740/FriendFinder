1fd1d1820cf5a711e935cdfa431dc494eafc4c64
Noel Nevarez <noel3740@gmail.com>
2019-01-17 11:33:52 -0600

    Adding more intial friends to the friends array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 // Friends data
-let fiends = [
+const friends = [
     {
         "routeName": "bradpitt",
         "name":"Brad Pitt",
-        "photo":"https://cdn1.thr.com/sites/default/files/imagecache/portrait_300x450/2012/05/brad_pitt_chanel_a_p.jpg",
+        "photo":"https://image.tmdb.org/t/p/w600_and_h900_bestv2/kU3B75TyRiCgE270EyZnHjfivoq.jpg",
         "scores":[
             5,
             1,
@@ -16,7 +16,160 @@ let fiends = [
             4,
             1
           ]
+      },
+      {
+        "routeName": "willsmith",
+        "name":"Will Smith",
+        "photo":"https://image.tmdb.org/t/p/w600_and_h900_bestv2/eze9FO9VuryXLP0aF2cRqPCcibN.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+          ]
+      },
+      {
+        "routeName": "johnnydepp",
+        "name":"Johnny Depp",
+        "photo":"https://image.tmdb.org/t/p/w600_and_h900_bestv2/tQfBM7GqgLSWYao1Ljq8xczgP6j.jpg",
+        "scores":[
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+          ]
+      },
+      {
+        "routeName": "tomhanks",
+        "name":"Tom Hanks",
+        "photo":"https://image.tmdb.org/t/p/w600_and_h900_bestv2/xxPMucou2wRDxLrud8i2D4dsywh.jpg",
+        "scores":[
+            3,
+            5,
+            1,
+            1,
+            2,
+            5,
+            3,
+            1,
+            2,
+            5
+          ]
+      },
+      {
+        "routeName": "scarlettjohansson",
+        "name":"Scarlett Johansson",
+        "photo":"https://image.tmdb.org/t/p/w600_and_h900_bestv2/tHMgW7Pg0Fg6HmB8Kh8Ixk6yxZw.jpg",
+        "scores":[
+            1,
+            3,
+            3,
+            3,
+            4,
+            2,
+            3,
+            4,
+            3,
+            2
+          ]
+      },
+      {
+        "routeName": "beyonceknowles",
+        "name":"Beyonce Knowles",
+        "photo":"https://image.tmdb.org/t/p/w600_and_h900_bestv2/cOFQlPKAmo2DlnZwMCYo3b7sI2K.jpg",
+        "scores":[
+            1,
+            2,
+            5,
+            5,
+            1,
+            2,
+            3,
+            1,
+            5,
+            2
+          ]
+      },
+      {
+        "routeName": "katherynwinnick",
+        "name":"Katheryn Winnick",
+        "photo":"https://image.tmdb.org/t/p/w600_and_h900_bestv2/qt458ga1sQFZ4SZUBxaknJa8fpg.jpg",
+        "scores":[
+            1,
+            5,
+            3,
+            1,
+            2,
+            1,
+            2,
+            1,
+            2,
+            1
+          ]
+      },
+      {
+        "routeName": "jasonmomoa",
+        "name":"Jason Momoa",
+        "photo":"https://image.tmdb.org/t/p/w600_and_h900_bestv2/pG1Faei9M8ObR4bOD2ljiSVuaDm.jpg",
+        "scores":[
+            5,
+            4,
+            3,
+            2,
+            1,
+            5,
+            4,
+            3,
+            2,
+            1
+          ]
+      },
+      {
+        "routeName": "haileesteinfeld",
+        "name":"Hailee Steinfeld",
+        "photo":"https://cdn1.thr.com/sites/default/files/imagecache/portrait_300x450/2012/05/brad_pitt_chanel_a_p.jpg",
+        "scores":[
+            1,
+            2,
+            3,
+            1,
+            2,
+            3,
+            4,
+            5,
+            1,
+            2
+          ]
+      },
+      {
+        "routeName": "henrycavill",
+        "name":"Henry Cavill",
+        "photo":"https://image.tmdb.org/t/p/w600_and_h900_bestv2/hErUwonrQgY5Y7RfxOfv8Fq11MB.jpg",
+        "scores":[
+            2,
+            3,
+            2,
+            3,
+            4,
+            5,
+            4,
+            5,
+            1,
+            2
+          ]
       }
 ];
 
-module.exports = fiends;
+module.exports = friends;
