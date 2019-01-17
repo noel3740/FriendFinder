83496cb10434727734001260857857be93c27788
Noel Nevarez <noel3740@gmail.com>
2019-01-16 21:11:49 -0600

    Adding handlebar code.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Add dependencies
 const express = require("express");
+const exphbs = require("express-handlebars");
 
 //Set up the express app
 const app = express();
@@ -9,10 +10,23 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Setup handlebars
+//Set custom layout and partials directory
+app.engine("handlebars", exphbs(
+    {
+        defaultLayout: "main",
+        layoutsDir: 'app/public/views/layouts',
+        partialsDir: 'app/public/views'
+    }));
+
+app.set("view engine", "handlebars");
+//Set a custom views folder directory
+app.set('views', __dirname + `/app/public/views`);
+
 //Server Routes
-const apiRoutes=require("./app/routing/apiRoutes");
+const apiRoutes = require("./app/routing/apiRoutes");
 apiRoutes(app);
-const htmlRoutes=require("./app/routing/htmlRoutes");
+const htmlRoutes = require("./app/routing/htmlRoutes");
 htmlRoutes(app);
 
 // Starts the server to begin listening
