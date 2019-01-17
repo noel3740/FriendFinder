7359910b436144e8eb3c7ee707c42a07b8e9c8f6
Noel Nevarez <noel3740@gmail.com>
2019-01-16 21:10:03 -0600

    Adding routes to the handlebar files.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,11 +6,11 @@ module.exports = function (app) {
 
     // Default route that sends the user first to the home page
     app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.render("home", { layout: "main" });
     });
 
     // Survey route that sends the user first to the survey page
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/survey.html"));
+        res.render("survey", { layout: "main" });
     });
 }
