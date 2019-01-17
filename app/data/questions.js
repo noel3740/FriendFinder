c95c4a76d8d825ecf16a62c8a74c5cb21bca2983
Noel Nevarez <noel3740@gmail.com>
2019-01-17 11:34:23 -0600

    Adding questions array

---
+++ app/data/questions.js
@@ -0,0 +1,45 @@
+// Questions data
+const questions = [
+    {
+        "id": 1,
+        "question": "You like action movies."
+    },
+    {
+        "id": 2,
+        "question": "You like to go to art museums."
+    },
+    {
+        "id": 3,
+        "question": "You like math."
+    },
+    {
+        "id": 4,
+        "question": "People can rarely upset you."
+    },
+    {
+        "id": 5,
+        "question": "It is often difficult for you to relate to other people’s feelings."
+    },
+    {
+        "id": 6,
+        "question": "You like long walks on the beach."
+    },
+    {
+        "id": 7,
+        "question": "You like sports."
+    },
+    {
+        "id": 8,
+        "question": "You like to camp."
+    },
+    {
+        "id": 9,
+        "question": "You like to hike."
+    },
+    {
+        "id": 10,
+        "question": "You have a vivid imagination."
+    }
+];
+
+module.exports = questions;
